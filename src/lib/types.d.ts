19d192ab0959d780dcc074a232420dd53d6cf359
Scott Brooks <scott.brooks@amplitude.com>
2021-08-23 13:58:33 -0700

    Created CVS export option

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -2,7 +2,7 @@ import { URL } from "url";
 
 export type Path = string;
 export type Sort = "score" | "churn" | "complexity" | "file" | "ratio";
-export type Format = "table" | "json";
+export type Format = "table" | "json" | "csv";
 export type Options = {
   target: string | URL;
   directory: string;
