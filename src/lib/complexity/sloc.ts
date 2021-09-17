9771e51e3ebfb1fdbf907f6c64c398f3691fde00
Simon Renoult <contact@simonrenoult.me>
2021-09-18 01:12:29 +0200

    fix(sloc): use node-sloc to preserve languages support

--- src/lib/complexity/sloc.ts
+++ src/lib/complexity/sloc.ts
@@ -1,27 +1,7 @@
-import * as fs from "fs";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const sloc = require("node-sloc");
 
-const getComments = (text: string): Array<string> => {
-  // Regex that matches all the strigns starting with //
-  const singleLineCommentsRegex = /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm;
-  return text ? text.match(singleLineCommentsRegex) || [] : [];
-};
-
-const calculateFromSourceCode = (sourceText: string): number => {
-  const comments = getComments(sourceText);
-  for (let i = 0; i < comments.length; i++) {
-    const aMatched = comments[i];
-    sourceText = sourceText.replace(aMatched, "").trim();
-  }
-
-  return sourceText
-    .split("\n")
-    .map((aLine) => aLine.trim())
-    .filter((aLine) => !!aLine).length;
-};
-
-export function calculate(path: string): number {
-  const contents = fs.readFileSync(path).toString("utf8");
-  const result = calculateFromSourceCode(contents);
-
-  return result;
+export async function calculate(path: string): Promise<number> {
+  const result = await sloc({ path });
+  return result ? result.sloc : 1;
 }
