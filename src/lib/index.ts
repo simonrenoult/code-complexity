be7836c44314d4f3da9711fba3949bf6ced6fee9
Ádám Liszkai <adam@liszkai.dev>
2024-02-18 02:29:41 +0000

    feat: add named exports
    Exposing the various types and classes for programatical usage
    with a shortcut for the complexity calculation. This should
    give a decent support for functional or object-oriented support
    while keeping the changes low.
    
    refs: #8

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,21 @@
 import Statistics from "./statistics/statistics";
+import Churns from "./churn/churns";
+import Complexities from "./complexity/complexities";
 
+// Expose shared type interfaces
+export * from "./types";
+
+// For CommonJS support: require("code-complexity")
 export default Statistics;
+
+// For ESM functional-style support: import { analyze } from "code-complexity";
+export const analyze = Statistics.compute;
+export const analyse = Statistics.compute;
+
+// For ESM object-style: `import { Statistics } from "code-complexity"`
+export {
+  Statistics,
+  // Expose these as they are dependencies for Statistics
+  Churns,
+  Complexities,
+};
